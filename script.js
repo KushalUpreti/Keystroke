6db413ce4c777073fc22b3b24ab1b45ceba7882b
Kushal Upreti <ehunt9030@gmail.com>
2020-08-15 21:56:59 +0545

    Added metrics

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 var array = null;
-var counter = 0;
 var currentText = null;
 var active = false;
 var easy = true;
-var count = 0;
+var counter = 0;
+var letterCount = 0;
+var totalWords = 0;
+var totalErrors = 0;
+var time = 0;
+var topSpeed = 0;
+var maxSpeed = 0;
+
+var averageArray = [];
+
+
+setInterval(function() {
+    if (active) {
+        time++;
+    }
+}, 1000);
 
 // This function generates random text everytime the page is refreshed or the old set of words are completed.
 function generateText() {
     var easyText = ["a", "s", "d", "w", "r", "t", "h", "j", "n", "m", "i", "o", "v", "e", "f"];
     var hardText = ["g", "h", "z", "x", "c", "b", "k", "y", "q", "p", "u", "n", "m", "v", "l"];
     var string = "";
-    count = 0;
+    letterCount = 0;
     while (true) {
         if (easy) {
             string = createWords(easyText, string);
         } else {
             string = createWords(hardText, string);
         }
-        if (count > 120) {
+        if (letterCount > 120) {
             break;
         }
     }
@@ -27,11 +41,14 @@ function generateText() {
 // This function is responsible for generating words. The words can be one to six letters long.
 function createWords(array, string) {
     var wordLength = Math.ceil(Math.random() * 6);
-    if (count > 0) { string = string + " "; }
+    if (letterCount > 0) {
+        string = string + " ";
+        totalWords++;
+    }
     for (var i = 0; i < wordLength; i++) {
         var character = Math.floor(Math.random() * 15);
         string += array[character];
-        count++;
+        letterCount++;
     }
     return string;
 }
@@ -47,7 +64,6 @@ function displayText(input) {
         if (val === " ") { val = "&nbsp;" }
         element.innerHTML += "<span>" + val + "</span>";
     }
-    console.log(array.length);
     flicker();
 }
 
@@ -65,6 +81,7 @@ document.addEventListener('keydown', function(event) {
 
         if (event.key !== array[counter]) {
             currentText.classList.add("textColor");
+            totalErrors++;
         }
         currentText.classList.toggle("flicker");
         counter++;
@@ -78,7 +95,6 @@ document.addEventListener('keydown', function(event) {
             document.querySelector(searchId).classList.toggle("anim2");
         }, 300)
     }
-    console.log(counter);
     if (counter == array.length) {
         reload();
     }
@@ -87,8 +103,52 @@ document.addEventListener('keydown', function(event) {
 // This function is used to generate new set of text and also brings up the previous stats.
 function reload() {
     counter = 0;
-    generateText();
+    calculateScore(time, totalWords);
     currentText.classList.remove("textColor");
+    time = 0;
+    totalWords = 0;
+    totalErrors = 0;
+
+
+    generateText();
+}
+
+function calculateScore(time, totalWords) {
+    var wpm = Math.floor(totalWords / (time / 60));
+    var totalScore = ((1200 - (totalErrors * 10)) + wpm * 2);
+
+    // The code below adds the new top wpm to the page.
+    let gain = document.querySelector("#speedGain");
+    if (wpm > topSpeed) {
+        topSpeed = wpm;
+        document.querySelector("#topSpeed").innerHTML = topSpeed + "<br>Wpm";
+    }
+
+    //Code below calculates the score increment or decrement from the previous one
+    if (wpm > maxSpeed) {
+        var speedGain = wpm - maxSpeed;
+        gain.innerHTML = speedGain + "<br>Wpm+";
+        gain.classList.remove("negativeStats");
+    } else {
+        var speedGain = maxSpeed - wpm;
+        gain.classList.add("negativeStats");
+        gain.innerHTML = speedGain + "<br>Wpm-";
+    }
+    maxSpeed = wpm;
+
+    // The code below produces the average score from all the attempts.
+    let len = averageArray.push(totalScore);
+    let sum = 0;
+    averageArray.forEach(function(item) {
+        sum = sum + item;
+    });
+    let finalaverage = Math.ceil(sum / len);
+
+
+    document.querySelector("#points").innerHTML = totalScore + "<br>points";
+    document.querySelector("#wpm").innerHTML = wpm + "<br>Wpm";
+    document.querySelector("#errors").innerHTML = totalErrors + "<br>errors";
+    document.querySelector("#average").innerHTML = finalaverage + "<br>points";
 }
 
 // This function is responsible for activating or deactivating the textbox. The timer stops when deactivated
